6f37b694dc1252cbed97a4950bfb4915d19f2bd3
Gabriel Andrade <gabriel.pereira1788@gmail.com>
2022-08-21 18:13:41 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h2>Hello </h2>
+    </div>
+  );
+}
+
+export default App;
+
